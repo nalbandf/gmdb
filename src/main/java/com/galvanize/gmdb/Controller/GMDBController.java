4c4e3df653064a4de9abfb90077e8c33199954f9
Addisalem Wordofa <addaber@gmail.com>
2021-06-05 11:56:06 -0400

    get empty movie test case

--- src/main/java/com/galvanize/gmdb/Controller/GMDBController.java
+++ src/main/java/com/galvanize/gmdb/Controller/GMDBController.java
@@ -1,4 +1,24 @@
 package com.galvanize.gmdb.Controller;
 
+import com.galvanize.gmdb.Domain.GMDBMovie;
+import com.galvanize.gmdb.Service.GMDBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gmdb")
 public class GMDBController {
+
+    @Autowired
+    GMDBService service;
+    @GetMapping("/movies")
+    public ResponseEntity<Iterable<GMDBMovie>>  getAllMovies(){
+        return new ResponseEntity(service.findAll(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/galvanize/gmdb/Service/GMDBService.java
+++ src/main/java/com/galvanize/gmdb/Service/GMDBService.java
@@ -1,4 +1,17 @@
 package com.galvanize.gmdb.Service;
 
+import com.galvanize.gmdb.Domain.GMDBMovie;
+import com.galvanize.gmdb.Repository.GMDBRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GMDBService {
+    @Autowired
+    GMDBRepository repository;
+    public Iterable<GMDBMovie> findAll(){
+        return repository.findAll();
+    }
 }
