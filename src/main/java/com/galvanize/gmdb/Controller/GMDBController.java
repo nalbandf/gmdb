bf36dcb17fc456222362ae1066f3f0715926da52
Addisalem Wordofa <addaber@gmail.com>
2021-06-05 13:43:26 -0400

    post movie test case-updated

--- src/main/java/com/galvanize/gmdb/Controller/GMDBController.java
+++ src/main/java/com/galvanize/gmdb/Controller/GMDBController.java
@@ -24,5 +24,5 @@ public class GMDBController {
     public ResponseEntity<GMDBMovie> createMovie(@RequestBody GMDBMovie movie){
         return new ResponseEntity(service.createMovie(movie),HttpStatus.CREATED);
     }
-    @
+    @Get
 }
